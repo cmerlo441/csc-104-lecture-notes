301717b9d2a9867413b553446a6c7a6d6e84ae52
Chris Merlo <cmerlo@ncc.edu>
2019-05-06 11:27:14 -0400

    Updated to Day 21

---
+++ hello-with-author-email.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+'''Display a friendly message.
+
+Display a friendly "Hello World" message to the user,
+confirming that the computer's Python installation is in good
+working order.
+
+This script is part of Homework #1, due 2019-09-09.
+'''
+
+__author__  = 'J. Student'
+__section__ = 'CSC 104 D1'
+__email__   = 'N00100100@students.ncc.edu'
+
+print("Hello world!")
---
+++ hello-with-docstring.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+'''Display a friendly message.
+
+Display a friendly "Hello World" message to the user,
+confirming that the computer's Python installation is in good
+working order.
+
+This script is part of Homework #1, due 2019-09-09.
+'''
